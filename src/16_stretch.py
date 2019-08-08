6e722d4e47ba64195c235e37f11d069c5eb22e33
Phil Smith <pbsmithtbn@gmail.com>
2019-08-07 23:03:45 -0500

    Finish sieve stretch

---
+++ src/16_stretch.py
@@ -0,0 +1,19 @@
+import math
+
+
+def is_prime(x):
+    if x == 1:
+        return False
+
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+        return True
+
+
+x = int(input("Enter a number: "))
+
+if is_prime(x):
+    print("It is prime")
+else:
+    print("It is not prime")
